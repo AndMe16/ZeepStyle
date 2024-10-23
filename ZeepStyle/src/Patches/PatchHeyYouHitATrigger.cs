177e4df765e6abde9d6212d96f38f8c39946245f
AndMe13 <afmendez99@outlook.com>
2024-10-22 20:37:18 -0500

    Change finish check

---
+++ ZeepStyle/src/Patches/PatchHeyYouHitATrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+using ZeepStyle;
+
+[HarmonyPatch(typeof(ReadyToReset), "HeyYouHitATrigger")]
+public class Patch_HeyYouHitATrigger
+{
+    // Event to notify when entering the method and checking the condition
+    public static event Action<bool> OnHeyYouHitATrigger;
+
+    // Harmony Postfix: Executes after the original method
+    static void Postfix(ReadyToReset __instance, bool isFinish)
+    {
+        if ((isFinish && __instance.actuallyFinished) && (__instance.master.currentLevelMode.HasThisPlayerFinishedAccountingForRacepoints(PlayerManager.Instance.currentMaster.playerResults.First())))
+        {
+            //Plugin.Logger.LogInfo("Actually finished the race!");
+            OnHeyYouHitATrigger?.Invoke(__instance.actuallyFinished);
+        }
+    }
+}
