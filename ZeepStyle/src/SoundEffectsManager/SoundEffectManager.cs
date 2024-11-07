cfb9314995de051b2c3830464ee90d49ecd7141f
AndMe13 <afmendez99@outlook.com>
2024-11-07 16:05:29 -0500

    Added Style_SoundEffectManager

---
+++ ZeepStyle/src/SoundEffectsManager/SoundEffectManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Style_SoundEffectManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+    }
+
+    public void PlaySound()
+    {
+        AudioClip clip = Resources.Load<AudioClip>("CD_00028");
+        if (clip != null)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.LogError("Audio clip not found in Resources!");
+        }
+    }
+}
